cf47033a7aa72284c0398dd41b1f83069e8a009c
Andreas Beger <adbeger@gmail.com>
2012-11-16 16:20:19 -0500

    worldMap world outline overlay

--- worldMap.R
+++ worldMap.R
@@ -1,11 +1,11 @@
 ##########
-# What: Function for chloropleth world map of PITF related data
+# What: Function for chloropleth world map
 # Date: October 2012
 # Who:  Andreas Beger
 #
 ###########
 
-worldMap <- function(x, id, data, date='2008-01-01') {
+worldMap <- function(x, id, data, date='2008-01-01', legend.title=NULL) {
   # Input 2-column matrix with unique identifier and data to be mapped for 
   # state slice in "date", output thematic map.
   
@@ -13,14 +13,16 @@ worldMap <- function(x, id, data, date='2008-01-01') {
   require(classInt)
   require(RColorBrewer)
   require(plyr)
+  require(shape) # for color legend
   
   # Validate input
   if (all(c(x, id) %in% colnames(data))==F) { stop('x or id not in data') }
-  if ((dim(data)[2]==2)==F) { stop('data must have 2 columns')}
   if (any(duplicated(data[, id]))==T) { stop('id is not unique in data') }
   
   # Load map data
   world <- cshp(date=as.Date(date))
+  world.outline <- cshp(date=as.Date(date))
+  world.outline <- unionSpatialPolygons(world.outline, world.outline@data$COWEYEAR)
   
   # Merge data into spatial object
   world@data <- data.frame(world@data, data[match(world@data[, 'COWCODE'], data[, id]), ])
@@ -32,28 +34,43 @@ worldMap <- function(x, id, data, date='2008-01-01') {
   if (nval==2) {
     colorpal <- c('#FEE0D2', '#EF3B2C')
     colors <- ifelse(is.na(world@data[, x])==T, '#B0B0B0', colorpal[(world@data[, x]+1)])
+    
+    # Plot map
+    plot(world, col='gray30', border='gray30', lwd=1)
+    plot(world, col=colors, border=F, add=T)
   }
   
   # 3 to 9 unique values
   if (nval >= 3 & nval <= 9) {
     colorpal <- brewer.pal(nval, 'Reds')
     colors <- ifelse(is.na(world@data[, x])==T, '#B0B0B0', colorpal[(world@data[, x]+1)])
+    
+    # Plot map
+    plot(world, col='grey', border='grey', lwd=1)
+    plot(world, col=colors, border=F, add=T)
   }
   
-  # Default (old legacy)
-  if (exists('colors')==F) {
+  # Continuous
+  if (nval > 9) {
     colorpal <- brewer.pal(9, 'Reds')
-    breaks <- c(0, 1, 2, 4, 6, 9, 12, 18, 24, 36)
-    colors <- colorpal[findInterval(world@data[, x], breaks)]
-    #colorpal <- brewer.pal(3, 'Reds')
-    #breaks <- c(0, 1, 2, 5)
+    colorpal <- colorRampPalette(colorpal)
+    maxy <- 100
+    breaks <- c(0, 10, 20, 50, 75, 100)
+    colors <- ifelse(is.na(world@data[, x])==T, '#B0B0B0', colorpal(maxy)[floor(world@data[, x]) + 1] )
+    
+    # Plot map
+    plot(world, col=colors, border='white', lwd=0.3)
+    plot(world.outline, border='gray50', add=T, lwd=0.8)
+    colorlegend(posy=c(0.1, 0.5), posx=c(0.1, 0.12),
+                col=colorpal(maxy), zlim=c(0, maxy), zval=breaks,
+                main=legend.title, main.cex=1, cex=1)
   }
-  # Need to add/finish code for 3to9 categories and continuous data
-  # What about continuous that spans zero?
-  # Color grey for missing values, remove this when above is done
-  colors <- ifelse(is.na(world@data[, x])==T, '#B0B0B0', colors)
+  #   ncol <- length(unique(world@data[, x]))
+  #   colorpal <- brewer.pal(ncol, 'Reds')
+  #   colors <- colorpal[world@data[, x]]
   
-  # Plot map
-  plot(world, col='grey', border='grey', lwd=1)
-  plot(world, col=colors, border=F, add=T)
+  # Legends?
+  # What about continuous that spans zero?
+  # how to pick breaks in cont. colorlegend
+  # transforming continuous data for plotting
 }
