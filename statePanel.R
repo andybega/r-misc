afd4e7d68abba0399e529d28d85cb45c7ba7594f
Andreas Beger <adbeger@gmail.com>
2013-12-10 17:40:52 -0500

    function for panel data + to date conversion

---
+++ statePanel.R
@@ -0,0 +1,59 @@
+################################################################################
+##
+##    Create a blank panel data set based on Gleditsch and Ward or COW.
+##    Andreas Beger
+##    10 Dec. 2013
+##
+##    G&W: http://privatewww.essex.ac.uk/~ksg/statelist.html
+##    COW: http://www.correlatesofwar.org/datasets.html
+##
+################################################################################
+
+statePanel <- function(start.date, end.date, by="month", useGW=TRUE) {
+  require(cshapes)
+  start.date <- attemptDate(start.date, by)
+  end.date <- attemptDate(end.date, by)
+  # Create vector of desired dates
+  date <- seq.Date(start.date, end.date, by=by)
+  # Initialize results panel
+  panel <- data.frame(NULL)
+  for (i in seq_along(date)) {
+    if (useGW) {
+      date.slice <- cshp(date=date[i], useGW=TRUE)$GWCODE
+    } else if (!useGW) {
+      date.slice <- cshp(date=date[i], useGW=FALSE)$COWCODE
+    }
+    # Append to results panel
+    panel <- rbind(panel, data.frame(ccode=date.slice, date=date[i]))
+  }
+  # Create unique ID
+  panel$id <- paste(panel$date, panel$ccode)
+  return(panel)
+}
+
+attemptDate <- function(date, by) {
+  if (!class(date)=="Date") {
+  	date <- tryCatch(as.Date(date), error=function(e) date)
+  	if (class(date)=="Date") {
+  	  warning("Converting to 'Date' class")
+  	} else if (by=="year") {
+  	  date <- tryCatch(as.Date(paste0(date, "-06-30")), error=function(e) date)
+  	  if (class(date)=="Date") {
+  	  	warning("Converting to 'Date' class with yyyy-06-30")
+  	  }
+  	} else if (by=="month") {
+  	  date <- tryCatch(as.Date(paste0(date, "-15")), error=function(e) date)
+  	  if (class(date)=="Date") {
+  	  	warning("Converting to 'Date' class with yyyy-mm-15")
+  	  }
+  	}
+  }
+  if (!class(date)=="Date") {
+  	stop(paste("Count not convert to class 'Date'"))
+  }
+  return(date)
+}
+
+# Example
+#example1 <- statePanel("2000", "2010", by="year")
+#example2 <- statePanel("1955-01", "2012-06", by="month")
